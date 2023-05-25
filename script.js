7931366895fb0a81585cc32ddf859d9b6d7b7eb3
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-25 17:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ function getFormvalue() {
     //Write your code here
 	let fname = document.getElementById("fname").value;
 	let lname = document.getElementById("lname").value;
-	alert(`First name: ${fname}
-Last name: ${lname}`);
+	alert(fname + " " +lname);
 }
